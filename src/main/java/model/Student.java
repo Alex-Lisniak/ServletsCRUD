8fc645254c517793d24989f0fb9dc910f454feac
Alex-Lisniak <lisnyak.am1010a@gmail.com>
2022-06-05 17:26:17 +0300

    Add student class

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Student {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    Student(String name , String surname , int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+}
