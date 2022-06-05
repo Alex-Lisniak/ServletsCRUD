08f447d9f455c89d0a4f4f954d9306ddd7b895db
Alex-Lisniak <lisnyak.am1010a@gmail.com>
2022-06-05 17:30:39 +0300

    Add override methods on Student

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,14 +1,37 @@
 package model;
 
+import java.util.Objects;
+
 public class Student {
+
+    private Long id;
 
     private String name;
     private String surname;
     private int age;
 
-    Student(String name , String surname , int age){
+    Student(Long id, String name , String surname , int age){
+        this.id = id;
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name='" + name + '\'' + ", surname='" + surname + '\'' + ", age=" + age + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && id.equals(student.id) && name.equals(student.name) && surname.equals(student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, age);
     }
 }
